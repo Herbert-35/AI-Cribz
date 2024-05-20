30242464917b7734a890906ac8505cff889c5bab
Herbert-35 <46800742+Herbert-35@users.noreply.github.com>
2024-05-19 22:29:00 -0700

    Update stf.py

--- stf.py
+++ stf.py
@@ -7,15 +7,12 @@ import argparse
 # parser.add_argument("-i", "--input", required=True, help="path to input image")
 # args = vars(parser.parse_args())
 config = {
-  "apiKey": "AIzaSyCCHb56XahmBYrbSh_jR1dhogZyav6muKg",
-  "authDomain": "project-cab30.firebaseapp.com",
-  "databaseURL": "https://project-cab30-default-rtdb.firebaseio.com",
-  "projectId": "project-cab30",
-  "storageBucket": "project-cab30.appspot.com",
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "projectId": "",
+  "storageBucket": "",
   "serviceAccount": "serviceAccountKey.json",
-  # "messagingSenderId": "903723741684",
-  # "appId": "1:903723741684:web:240274745bb432ffbe09a1",
-  # "measurementId": "G-KQ928CZK2W"
 }
 
 firebase = pyrebase.initialize_app(config)
